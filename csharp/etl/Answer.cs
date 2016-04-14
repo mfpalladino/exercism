fa525a23c555a306b3770a3ef467582fecec9af6
Marcelo Palladino <mpalladino@directtalk.com.br>
2016-04-14 12:34:43 -0300

    exercism csharp etl.

---
+++ csharp/etl/Answer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class ETL
+{
+    public static Dictionary<string, int> Transform(Dictionary<int, IList<string>> old)
+    {
+        var transformedData = new Dictionary<string, int>();
+        old.Keys.Foreach(oldKey => old[oldKey].Foreach(oldValue => transformedData.Add(oldValue.ToLower(), oldKey)));
+        return transformedData;
+    }
+}
+
+public static class EnumerableExtensions
+{
+    public static void Foreach<T>(this IEnumerable<T> source, Action<T> action)
+    {
+        foreach (var item in source)
+            action(item);
+    }
+}
